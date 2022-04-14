9e2658e3acd34f3048cb1ed498ff826aa061f73e
Angel Senra <git@angelsenra.com>
2022-04-13 20:40:52 -0500

    Follow Django tutorial part 5

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 
@@ -6,6 +9,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
