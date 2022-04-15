d4f3f3482181a18aff6a9ddba1c831dfea114eb2
Angel Senra <git@angelsenra.com>
2022-04-14 21:09:43 -0500

    Delete priorities. They can be replicated with categories so far. If at some point we need something more complex we can add them back.

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from colorfield.fields import ColorField
 import uuid
 
-class Priority(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=200)
-    order = models.IntegerField()
-    color = ColorField(default='#FF0000')
-
-    def __str__(self):
-        return f"{self.name} - {self.order}"
-
-
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now=True)
@@ -21,7 +9,6 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     order = models.IntegerField()
     color = ColorField(default='#FF0000')
-    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
@@ -35,9 +22,8 @@ class Expense(models.Model):
     amount = models.FloatField()
     source = models.CharField(max_length=200)
     spent_at = models.DateTimeField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # TODO should be set to some "other" category
     notes = models.CharField(max_length=1000, blank=True, null=True)
-    priority = models.ForeignKey(Priority, null=True, blank=True, on_delete=models.SET_NULL)
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
