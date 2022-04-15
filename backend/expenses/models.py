89e1d4bec3bddd754aea89df9e92cee84afe53f5
Angel Senra <git@angelsenra.com>
2022-04-15 07:36:01 -0500

    Add a very basic table with the expenses - Missing filtering by period (among other things)

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -1,6 +1,11 @@
+import typing
+import itertools
 from django.db import models
 from colorfield.fields import ColorField
+import logging
 import uuid
+
+logger = logging.getLogger(__name__)
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -9,10 +14,55 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     order = models.IntegerField()
     color = ColorField(default='#FF0000')
-    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL)
+    parent = models.ForeignKey("self", related_name="children", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f"{self.name} - {self.order}"
+        if self.parent:
+            return f"{self.name} (child of {self.parent.name})"
+        else:
+            return f"{self.name}"
+
+    def build_levels(self, iteration=0) -> tuple[list[list[tuple["Category", bool]]], int]:
+        if iteration > 50:
+            raise Exception(f"We hit 50 iterations on the recursive levels call. There might be something wrong with {self}")
+
+        if self is None:
+            children = Category.objects.filter(parent=None).all()
+            levels = list()
+        else:
+            children = self.children.all()
+            levels = [[(self, bool(children))]]
+
+        children_levels = [c.build_levels(iteration=iteration + 1)[0] for c in children]
+        if children_levels:
+            for i in range(max([len(cl) for cl in children_levels])):
+                li = list()
+                for cl in children_levels:
+                    if len(cl) <= i:
+                        continue
+                    li.extend(cl[i])
+                levels.append(li)
+        columns = sum(len(l) + len([i for i in l if i[1]]) for l in levels[::-1])
+        return levels, columns
+
+    def build_values(self, iteration=0) -> list[tuple[int, bool]]:
+        if iteration > 50:
+            raise Exception(f"We hit 50 iterations on the recursive levels call. There might be something wrong with {self}")
+
+        if self is None:
+            children = Category.objects.filter(parent=None).all()
+            other = 0
+        else:
+            children = self.children.all()
+            other = sum(expense.amount for expense in self.expenses.all())
+
+        if not children:
+            return [(other, False)]
+
+        children_values = list(itertools.chain(*[child.build_values(iteration=iteration + 1) for child in children]))
+        if self is None:
+            return children_values
+        return [(other + sum(v[0] for v in children_values if not v[1]), True), *children_values, (other, False)]
 
 
 class Expense(models.Model):
@@ -22,7 +72,7 @@ class Expense(models.Model):
     amount = models.FloatField()
     source = models.CharField(max_length=200)
     spent_at = models.DateTimeField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # TODO should be set to some "other" category
+    category = models.ForeignKey(Category, related_name="expenses", on_delete=models.CASCADE)
     notes = models.CharField(max_length=1000, blank=True, null=True)
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL)
 
--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from itertools import zip_longest
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.http import HttpResponse
+from .models import Category
 
-# Create your views here.
+
+def index(request):
+    return HttpResponseRedirect(reverse("expenses:table"))
+
+
+def table(request):
+    category_list = Category.objects.order_by("order").all()
+
+    levels, _ = Category.build_levels(None)
+    parents_considered = list()
+    header_rows = list()
+    for a, level in enumerate(levels):
+        levels_left = len(levels) - a
+        header_row = list()
+        for category, has_children in level:
+            parent = category.parent
+            if parent and parent not in parents_considered:
+                header_row.append(["Total", 1, levels_left])
+            _, columns_under_category = category.build_levels()
+            header_row.append([category.name, columns_under_category, 1 if has_children else levels_left])
+            if parent:
+                parents_considered.append(parent)
+                if parent.children.count() == parents_considered.count(parent):
+                    header_row.append(["Other", 1, levels_left])
+        header_rows.append(header_row)
+
+    parents_considered = list()
+    value_rows = list()
+    value_rows.append(["Apr22", [value for value, _ in Category.build_values(None)]])
+    value_rows.append(["May22", [0] * 13])
+    value_rows.append(["Jun22", [1] * 13])
+
+    context = {"category_list": category_list, "header_rows": header_rows, "value_rows": value_rows}
+    return render(request, "expenses/table.html", context)
