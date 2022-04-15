512e9388baa530afd54fdf1b78d263b4cf350201
Angel Senra <git@angelsenra.com>
2022-04-14 20:39:55 -0500

    Install colorfield module

--- backend/cashflow/settings.py
+++ backend/cashflow/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'colorfield',
+    'expenses.apps.ExpensesConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
