971e693bab0a275b90216f250f0b67ff4e268d65
Angel Senra <git@angelsenra.com>
2022-04-17 16:10:29 -0500

    Move core files out of folder

--- backend/settings.py
+++ backend/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "core.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -87,7 +87,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "core.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 
 # Database
